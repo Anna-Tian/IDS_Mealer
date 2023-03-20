ca22dad623d882cabdcbe14f644e9d4137ea4758
Anna-Tian <37281865+Anna-Tian@users.noreply.github.com>
2023-03-20 23:46:38 +1100

    Initial Commit

---
+++ Mealer/MealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealerApp.swift
+//  Mealer
+//
+//  Created by Na Tian on 20/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct MealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
